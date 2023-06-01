2a37fc700d23c45809a0eb50d2cb91f25c643265
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-06-01 12:05:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.89 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,9 @@
 def solution(participant, completion):
     participant.sort()
     completion.sort()
-    
-    for i in range(len(completion)):
-        if  participant[i] != completion[i] :
-            return participant[i]
+
+    for p, c in zip(participant, completion):
+        if  p != c:
+            return p
         
     return participant[-1]
