19a72ede283a4522a46b92089a4fe15ec0b246bc
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-06-07 10:29:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,10 @@
+def solution(n, arr1, arr2):
+    answer = []
+  
+    for i in range(n):
+        answer.append(str(bin(arr1[i] | arr2[i]))[2:].zfill(n))
+        
+        answer[i] = answer[i].replace('1', '#')
+        answer[i] = answer[i].replace('0', ' ')
+        
+    return answer
