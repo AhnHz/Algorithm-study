bc2cef6387215bf2a686cbc6a4ebd33b1ccb6486
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-07-05 18:15:14 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,19 +1,18 @@
 def solution(progresses, speeds):
-    cnt = 0
-    loop = 0
+    cnt = 0     # 개발된 기능 개수
+    loop = 0    # 시간
     answer = []
     
     while len(progresses) > 0:
-        if (progresses[0] + loop * speeds[0]) >= 100:
+        if (progresses[0] + loop * speeds[0]) >= 100:   # 진도율 업
             progresses.pop(0)
             speeds.pop(0)
             cnt += 1
             
         else:
             if cnt:
-                answer.append(cnt)
-                cnt = 0 
-            loop += 1
-    answer.append(cnt)       
+                answer.append(cnt)  # 개발된 기능이 있다면 정답 리스트에 개수 추가
+                cnt = 0     # 개수 초기화
+            loop += 1   # day + 1
+    answer.append(cnt)  # 마지막 개발 완료       
     return answer
-    
