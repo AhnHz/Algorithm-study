258e69b012c71da0726b2706659dc175bfc1848f
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-07-11 16:22:11 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 516 ms, Memory: 115292 KB -BaekjoonHub

--- 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
@@ -1,15 +1,12 @@
-import sys
-
-n, c = map(int, sys.stdin.readline().split())   # input 대신 sys.stdin.readline
-num = list(map(int, sys.stdin.readline().split()))
+n, c = map(int, input().split())
+num = list(map(int, input().split()))
 cnt = 0
 
-for i in range(n-1):
-    for j in range(n-1):
+for i in range(n-1):    # 자릿수마다 반복
+    for j in range(n-1):    # 처음부터 끝까지 교환 한번씩
         if num[j] > num[j+1]:
             num[j], num[j+1] = num[j+1], num[j]
-            cnt+=1
-
+            cnt+=1      # 교환횟수 카운트
             if cnt == c:
                 print(num[j], num[j+1])
                 quit()
