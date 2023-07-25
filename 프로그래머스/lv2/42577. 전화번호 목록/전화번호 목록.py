9c57d1318f979aa585c550b65791af77c9a0b539
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-07-25 14:58:17 +0900

    [level 2] Title: 전화번호 목록, Time: 96.02 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,9 @@
 def solution(phone_book):
-    phone_book.sort()
+    phone_book.sort()   # 오름차순 정렬. 인접한 전화번호끼리만 비교하면 됨
     
     for i in range(len(phone_book)-1):
+        # 기준이 되는 전화번호와 그 다음 전화번호 앞자리를 비교
         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False
+            return False    # 참이면 false를 반환하고 종료
             
     return True
