7e261904c69ba6e78ab9cbd99dba22fd6fb01d3c
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-08-28 15:54:45 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,13 +1,12 @@
 def solution(n, lost, reserve):
-    
     lost_uni = set(lost) - set(reserve)
     reserve_uni = set(reserve) - set(lost)
     
     for i in reserve_uni:
-        if i - 1 in lost_uni:
-            lost_uni.remove(i-1)
+        if i - 1 in lost_uni:       # 앞번호 학생이 체육복이 없는 경우
+            lost_uni.remove(i-1)    # 앞번호 학생은 체육복을 빌릴 수 있음
         
-        elif i + 1 in lost_uni:
-            lost_uni.remove(i+1)            
+        elif i + 1 in lost_uni:     # 뒷번호 학생이 없는 경우
+            lost_uni.remove(i+1)     
         
-    return n - len(lost_uni) 
+    return n - len(lost_uni)        # 전체 학생 - 체육복이 없는 학생
