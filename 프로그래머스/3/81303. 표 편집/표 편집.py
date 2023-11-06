ca3faae8d8ab8c693cac6e462a7a3bb026d6b551
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-11-07 01:23:16 +0900

    [level 3] Title: 표 편집, Time: 1204.06 ms, Memory: 315 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.py
+++ 프로그래머스/3/81303. 표 편집/표 편집.py
@@ -11,7 +11,6 @@ def solution(n, k, cmd):
     
     # linked list
     table = {i: ListNode(i-1, i+1) for i in range(n)}  # 0 ~ n
-    #print(table)
     
     now = k  # 커서 위치
     delete = []  # 삭제한 행
@@ -20,24 +19,23 @@ def solution(n, k, cmd):
     
     def move_down(X, now):        
         for _ in range(X):
-            if table[now].down is not None:
+            if table[now].down is not None:  # 아래 행이 존재함
                 now = table[now].down
-            else:  # down is None
+            else:  # down is None: 현재 행이 마지막 행
                 while table[now].up is not None:  # up이 None이 아닐 경우
-                    now = table[now].up
+                    now = table[now].up  # 커서를 첫번째 행으로
         return now
             
     def move_up(X, now):        
         for _ in range(X):
-            if table[now].up is not None:
+            if table[now].up is not None:  # 윗 행이 존재함
                 now = table[now].up
-            else:   # up is None
+            else:   # up is None: 현재 행이 첫번째 행
                 while table[now].down is not None:  # down이 None이 아닐 경우
-                    now = table[now].down
+                    now = table[now].down  # 커서를 마지막 행으로
         return now
     
     for command in cmd:
-        #print(command)
         if command == 'C':
             up, down = table[now].up, table[now].down
             delete.append(now)  # 삭제된 행 리스트에 추가
@@ -45,16 +43,16 @@ def solution(n, k, cmd):
             if up is not None:
                 table[up].down = down 
                 
-            else:  # up is None:
-                table[down].up = None
+            else:  # up is None: 현재 행이 첫번째 행
+                table[down].up = None  # 삭제된 후, 아래 행을 첫번째 행으로 설정
                 
             if down is not None:
                 table[down].up = up
                 now = down  # 아래 행으로 커서 이동
                 
-            else:  # down is None
-                table[up].down = None
-                now = up                         
+            else:  # down is None: 현재 행이 마지막 행
+                table[up].down = None  # 삭제된 후, 윗 행을 마지막 행으로 설정
+                now = up  # 윗 행으로 커서 이동                         
                 
         elif command == 'Z':
             z = delete.pop()  # 최근 삭제 행 반환 후, 삭제
@@ -73,10 +71,8 @@ def solution(n, k, cmd):
                 now = move_up(int(X), now)
             else:  # 'D'
                 now = move_down(int(X), now)
-        #print(now)
-        #print(delete)
-    #print(table)    
+     
     for x in delete:
-        answer[x] = 'X'
+        answer[x] = 'X'  # 삭제된 행을 인덱스로 찾아 'X'로 변경
                           
     return ''.join(answer)
